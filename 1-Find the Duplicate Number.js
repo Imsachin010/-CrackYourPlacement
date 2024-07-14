c81ccf60052f25f07ac3a1852a1e36feca87c8b0
Sachin <007mishrasachinmishra@gmail.com>
2024-07-14 21:25:08 +0530

    Solvd a dsa problem

--- 1-Find the Duplicate Number.js
+++ 1-Find the Duplicate Number.js
@@ -5,3 +5,20 @@
  * @return {number}
  */
 
+var findDuplicate = function(nums) {
+    let fast = nums[0]
+    let slow = nums[0]
+    
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast)
+
+    slow = nums[0]
+    while (slow !== fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return fast;
+    
+};
