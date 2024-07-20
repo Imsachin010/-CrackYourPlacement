93f0225353eaccc90f78ce96ff569fe3b2637994
Sachin <007mishrasachinmishra@gmail.com>
2024-07-20 09:51:45 +0530

    solved

--- 8-Merge Sorted Array.js
+++ 8-Merge Sorted Array.js
@@ -3,3 +3,28 @@
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 // solution
+var merge = function(nums1, m, nums2, n) {
+    let p1 = m - 1; // Pointer for nums1
+    let p2 = n - 1; // Pointer for nums2
+    let p = m + n - 1; // Pointer for placement in nums1
+
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+
+    // If there are still elements in nums2, add them to nums1
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+        p--;
+    }
+};
+
+
