57936f3465f83989abf5ade115f4e20399442a42
Sachin <007mishrasachinmishra@gmail.com>
2024-07-20 09:52:21 +0530

    taken test case

--- 8-Merge Sorted Array.js
+++ 8-Merge Sorted Array.js
@@ -27,4 +27,9 @@ var merge = function(nums1, m, nums2, n) {
     }
 };
 
+// Example usage:
+let nums1 = [1,2,3,0,0,0];
+let nums2 = [2,5,6];
+merge(nums1, 3, nums2, 3);
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
 
