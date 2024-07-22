7464531fa3a0d208b66dfcd7622b7f896c7bc482
Sachin <007mishrasachinmishra@gmail.com>
2024-07-22 06:17:22 +0530

    solving approaches

--- 10-Majority Element.js
+++ 10-Majority Element.js
@@ -1,1 +1,8 @@
-// Problem: Given an array nums of size n, return the majority element.The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+// Problem: Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Solution : 
+// brute force - taking that element which is > n/2 times.Counting occurence of each element
+// example [2,2,1,3,1,2,2] n=7, so n/2 = 3.5 we'll consider that element which has n =4 which is 2 
+// Approach 2 : sorting elements, majority element will occupy 50% of array, always pass through n/2
+// sorting and loking the middle element of the array 
