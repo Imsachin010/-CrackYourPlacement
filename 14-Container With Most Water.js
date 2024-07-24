ff7edd7b06d7340873267ebbf2e2839e0ca43de3
Sachin <007mishrasachinmishra@gmail.com>
2024-07-24 19:55:51 +0530

    solved

--- 14-Container With Most Water.js
+++ 14-Container With Most Water.js
@@ -6,3 +6,24 @@
 
 // Solution 
 // Approach 1 - Brute Force
+// Approach 2 - Two Pointer
+
+var maxArea = function(height) {
+    let result = 0;
+    let l = 0;
+    let r = height.length - 1;
+
+    while(l < r){
+        area = (r - l) * Math.min(height[l], height[r])
+        result = Math.max(result, area)
+
+        if( height[l] < height[r]){
+            l += 1;
+        } else if(height[l] > height[r]){
+            r -= 1;
+        } else{
+            r-=1;
+        }
+    }
+    return result
+};
