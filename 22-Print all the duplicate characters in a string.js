93939918edea70e117aa15e37fa266c439e459c1
Sachin <007mishrasachinmishra@gmail.com>
2024-07-28 11:46:45 +0530

    problem 22

---
+++ 22-Print all the duplicate characters in a string.js
@@ -0,0 +1,6 @@
+// Problem : Print all the duplicate characters in a string
+// Given a string S, the task is to print all the duplicate characters with their occurrences in the given string.
+// Solution 
+// Approach 1: sort the string elements, with an element count increment it with the duplicate occuring element.
+// Approach 2: using hashing, to take counts and store them.
+
