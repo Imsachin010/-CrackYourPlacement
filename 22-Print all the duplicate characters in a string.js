f695f0a36908bdf6d1a8cde72e39ffaa8d64ee27
Sachin <007mishrasachinmishra@gmail.com>
2024-07-28 11:46:56 +0530

    solved

--- 22-Print all the duplicate characters in a string.js
+++ 22-Print all the duplicate characters in a string.js
@@ -4,3 +4,20 @@
 // Approach 1: sort the string elements, with an element count increment it with the duplicate occuring element.
 // Approach 2: using hashing, to take counts and store them.
 
+function printDuplicate(str){
+    str = str.split('').sort().join('');
+    for(let i=0; i < str.length; i++){
+        let count = 1;
+
+        while(i < str.length-1 && str[i] == str[i+1]){
+            count++;
+            i++;
+        }
+        if(count > 1){
+            console.log(str[i] + " counts equal to " + count);
+        }
+    }
+    
+};
+
+console.log(printDuplicate('RajaRamMohanRoy'));
