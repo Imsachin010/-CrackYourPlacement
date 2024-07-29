0b995ed41bbe9b8b252858f66146fec74496a27b
Sachin <007mishrasachinmishra@gmail.com>
2024-07-29 05:31:22 +0530

    solved

--- 23-Find the Index of the First Occurrence in a String.js
+++ 23-Find the Index of the First Occurrence in a String.js
@@ -2,3 +2,17 @@
 
 //Solution
 // Approach 1 - using nested loop for given characters and matching with the sub string
+
+var strStr = function(haystack, needle) {
+    if(haystack === needle || needle === "") return 0;
+
+    for(let i = 0; i < haystack.length; i++ ){
+        if(haystack[i] = needle[0]){
+            let sub = haystack.substring(i, i+needle.length);
+            if(sub === needle){
+                return i
+            }
+        }
+    }
+    return -1;
+};
