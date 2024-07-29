37a41d0f1ce4be9b6d5106a78914b1b278c889e0
Sachin <007mishrasachinmishra@gmail.com>
2024-07-29 05:30:35 +0530

    23

---
+++ 23-Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,4 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+//Solution
+// Approach 1 - using nested loop for given characters and matching with the sub string
