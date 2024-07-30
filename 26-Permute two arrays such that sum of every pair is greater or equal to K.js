f6f90957863e229e9159ae55adc9d2316a101157
Sachin <007mishrasachinmishra@gmail.com>
2024-07-30 22:07:26 +0530

    solved

--- 26-Permute two arrays such that sum of every pair is greater or equal to K.js
+++ 26-Permute two arrays such that sum of every pair is greater or equal to K.js
@@ -6,3 +6,15 @@
 // Solution : 
 // Approach 1 - sorting both the arrays and checking if sum is less than K, then will return false else no
 // Approach 2 - using two pointers to check if sum is greater than K
+
+function isEqualSum(a1, a2, n,k){
+    a1.sort((a,b) => a - b);
+    a2.sort((a,b) => a - b);
+
+    for(let i = 0; i < n; i++){
+        if(a1[i] + a2[i] < k) return "false";
+    }
+    return "true";
+}
+
+console.log(isEqualSum([2, 1, 3], [ 7, 8, 9], 3, 10)); //Yes
