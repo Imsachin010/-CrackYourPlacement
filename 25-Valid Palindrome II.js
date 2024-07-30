4c9bca28af624dbfe3886c5f3a8538c04f4ff452
Sachin <007mishrasachinmishra@gmail.com>
2024-07-30 21:49:44 +0530

    solved

--- 25-Valid Palindrome II.js
+++ 25-Valid Palindrome II.js
@@ -3,3 +3,30 @@
 // SOlution:
 // Approach 1 - start deleting a character at a timeand using algo to check if it is palindrome
 // Approach 2 - using two pointers to check if it is palindrome
+var validPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    const isPalindrome = (str, left, right) => {
+        while (left < right) {
+            if (str.charAt(left) !== str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    };
+    while(left < right){
+        if(s.charAt(left) === s.charAt(right)){
+            left++;
+            right--;
+        }
+        else{
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
+    }
+    return true;
+};
+
+console.log(validPalindrome("abca")); //true
