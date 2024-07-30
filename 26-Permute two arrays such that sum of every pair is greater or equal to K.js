dfd33fac11739db78036c0cb458956f3893d50a6
Sachin <007mishrasachinmishra@gmail.com>
2024-07-30 21:57:54 +0530

    26

--- 26-Permute two arrays such that sum of every pair is greater or equal to K.js
+++ 26-Permute two arrays such that sum of every pair is greater or equal to K.js
@@ -4,3 +4,5 @@
 // The task is to print “Yes” if any such permutation exists, otherwise print “No”.
 
 // Solution : 
+// Approach 1 - sorting both the arrays and checking if sum is less than K, then will return false else no
+// Approach 2 - using two pointers to check if sum is greater than K
