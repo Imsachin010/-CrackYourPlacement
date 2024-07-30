b56c7b86507572d518b6464b53f5fd27fbfb0462
Sachin <007mishrasachinmishra@gmail.com>
2024-07-30 19:44:51 +0530

    24

---
+++ 24-Longest Common Prefix.js
@@ -0,0 +1,8 @@
+// Problem : Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+// Solution
+// Approach 1 - using a nested loop for given characters & match with the sub sting
+// Approach 2 - using sort and compare with the last
+// Approach 3 - 
