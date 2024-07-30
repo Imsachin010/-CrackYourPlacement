e82d68516c3528e46b465cedd2f64dce2fa1c506
Sachin <007mishrasachinmishra@gmail.com>
2024-07-30 21:21:46 +0530

    solved

--- 24-Longest Common Prefix.js
+++ 24-Longest Common Prefix.js
@@ -5,4 +5,23 @@
 // Solution
 // Approach 1 - using a nested loop for given characters & match with the sub sting
 // Approach 2 - using sort and compare with the last
-// Approach 3 - 
+
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    strs.sort();
+    let first = strs[0]
+    let last = first.length;
+
+    for(let i =0; i < strs.length; i++){
+        let s = strs[i];
+
+        while(s.substring(0, last) !== first){
+            last--;
+            if(last === 0) return "";
+            first = first.substring(0, last)
+        }
+    }
+    return first;
+};
+console.log(longestCommonPrefix("flower","flow","flight"))
