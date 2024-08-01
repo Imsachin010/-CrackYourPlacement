e2511aca5b3e5819d916b7808faa6c8eb89fd513
Sachin <007mishrasachinmishra@gmail.com>
2024-08-01 19:46:31 +0530

    solved

--- 28-Find Pair Given Difference.js
+++ 28-Find Pair Given Difference.js
@@ -4,3 +4,23 @@
 
 // Solution 
 /// Approach 1 - brute force - using nested loop to chwck all possible pairs and return 1 if the difference is x
+
+
+// class Solution {
+//     /**
+//     * @param number n
+//     * @param number x
+//     * @param number[] arr
+
+//     * @returns number
+//     */
+//     findPair(n, x, arr) {
+//         // code here
+//         for(let i=0; i < n; i++){
+//             for(let j=1; j< n; j++){
+//                 if(arr[j] - arr[i] = x) return 1;
+            
+//             }
+//         }
+//         return -1
+// }}
