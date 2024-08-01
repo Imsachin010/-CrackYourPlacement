b6c0c111315e93a4f90e9a5a59554bdce88b73e8
Sachin <007mishrasachinmishra@gmail.com>
2024-08-01 19:39:45 +0530

    28

---
+++ 28-Find Pair Given Difference.js
@@ -0,0 +1,6 @@
+// Problem : Find Pair Given Difference
+// Given an array arr[] of size n and an integer x, return 1 
+// if there exists a pair of elements in the array whose absolute difference is x, otherwise, return -1.
+
+// Solution 
+/// Approach 1 - brute force - using nested loop to chwck all possible pairs and return 1 if the difference is x
