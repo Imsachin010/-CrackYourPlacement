3d4582d849d387b1267cb362b61fed64c029e285
Sachin <007mishrasachinmishra@gmail.com>
2024-08-02 21:43:10 +0530

    solved

--- 29-Middle of the Linked List.js
+++ 29-Middle of the Linked List.js
@@ -4,3 +4,27 @@
 // If there are two middle nodes, return the second middle node.
 
 // SOLUTION:
+// Approach 1: brute force - using loop to ttraverse the LL and store the values in an array and return th middle element of the array
+// approach 2 : using two pointer slow and faster to find the middle element 
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head, fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+    }
+    return slow
+};
+console.log(middleNode([1,2,3,4,5])) // 3
