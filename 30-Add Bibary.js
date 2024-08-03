2664fd97e87ede34a253aa828c75778d3b50c929
Sachin <007mishrasachinmishra@gmail.com>
2024-08-03 08:46:42 +0530

    30

---
+++ 30-Add Bibary.js
@@ -0,0 +1,4 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+// Solution:
+// Approach 1 : taking the binary no. from RHS, convert them to Integer number and adding them and then adding the carry
