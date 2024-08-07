384ee58d4452ff172259cf1399a6d2f15e9d8aa1
Sachin <007mishrasachinmishra@gmail.com>
2024-08-07 23:26:13 +0530

    solved

--- 31-Check if reversing a sub array make the array sorted.js
+++ 31-Check if reversing a sub array make the array sorted.js
@@ -1,4 +1,56 @@
-// Given an array of n distinct integers. 
-// The task is to check whether reversing any one sub-array can make the array sorted or not. 
+// Given an array of n distinct integers.
+// The task is to check whether reversing any one sub-array can make the array sorted or not.
 // If the array is already sorted or can be made sorted by reversing any one subarray, print “Yes“, else print “No“.
 
+function checkReverse(arr, n) {
+  // Copying the array.
+  let temp = [];
+  for (let i = 0; i < n; i++) {
+    temp[i] = arr[i];
+  }
+
+  // Sort the copied array.
+  temp.sort();
+
+  // Finding the first mismatch.
+  let front;
+  for (front = 0; front < n; front++) {
+    if (temp[front] != arr[front]) {
+      break;
+    }
+  }
+
+  // Finding the last mismatch.
+  let back;
+  for (back = n - 1; back >= 0; back--) {
+    if (temp[back] != arr[back]) {
+      break;
+    }
+  }
+
+  // If whole array is sorted
+  if (front >= back) {
+    return true;
+  }
+
+  // Checking subarray is decreasing or not.
+  do {
+    front++;
+    if (arr[front - 1] < arr[front]) {
+      return false;
+    }
+  } while (front != back);
+
+  return true;
+}
+
+// Driver Code
+
+let arr = [1, 2, 5, 4, 3];
+let n = arr.length;
+
+if (checkReverse(arr, n)) {
+  document.write("Yes");
+} else {
+  document.write("No");
+}
