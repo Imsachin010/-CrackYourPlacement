5b6523660028cd343212335445bc8a2689249df9
Sachin <007mishrasachinmishra@gmail.com>
2024-08-07 23:14:07 +0530

    31

---
+++ 31-Check if reversing a sub array make the array sorted.js
@@ -0,0 +1,4 @@
+// Given an array of n distinct integers. 
+// The task is to check whether reversing any one sub-array can make the array sorted or not. 
+// If the array is already sorted or can be made sorted by reversing any one subarray, print “Yes“, else print “No“.
+
