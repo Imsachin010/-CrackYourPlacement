724c25cad4753188734778069d4c59a13e681df0
Sachin <007mishrasachinmishra@gmail.com>
2024-08-08 17:50:33 +0530

    solved

--- 32-Linked List Cycle.js
+++ 32-Linked List Cycle.js
@@ -4,3 +4,19 @@
 // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
 // Return true if there is a cycle in the linked list. Otherwise, return false.
+// Solution: 
+// Approach 1 - Two pointer using fast and slow 
+
+var hasCycle = function (head) {
+    if(!head) return false;
+    let slow = head;
+    let fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+};
