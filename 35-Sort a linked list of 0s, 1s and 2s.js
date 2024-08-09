745ac122e849ab7437edaf12f9a6865e860f3170
Sachin <007mishrasachinmishra@gmail.com>
2024-08-09 23:32:12 +0530

    solved

--- 35-Sort a linked list of 0s, 1s and 2s.js
+++ 35-Sort a linked list of 0s, 1s and 2s.js
@@ -1,7 +1,35 @@
 // problem : Sort a linked list of 0s, 1s and 2s
 // Given a linked list of 0s, 1s and 2s, The task is to sort and print it.
 
-// Examples: 
+// Examples:
 
-// Input: 1 -> 1 -> 2 -> 0 -> 2 -> 0 -> 1 -> NULL 
+// Input: 1 -> 1 -> 2 -> 0 -> 2 -> 0 -> 1 -> NULL
 // Output: 0 -> 0 -> 1 -> 1 -> 1 -> 2 -> 2 -> NULL
+
+// Solution:
+// linkedlist
+class Node {
+  constructor(newData) {
+    this.data = newData;
+    this.next = null;
+  }
+}
+
+let cnt = [0, 0, 0];
+let ptr = head;
+while (ptr != null) {
+  cnt[ptr.data] += 1;
+  ptr = ptr.next;
+}
+let index = 0;
+ptr = head;
+
+while (ptr !== null) {
+  if (cnt[index] === 0) {
+    idx += 1;
+  } else {
+    ptr.data = index;
+    cnt[index] -= 1;
+    ptr = ptr.next;
+  }
+}
