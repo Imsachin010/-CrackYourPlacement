1c7a2ae6dc8d93cc87f7084836abe68ce6efc752
Sachin <007mishrasachinmishra@gmail.com>
2024-08-09 23:02:35 +0530

    34

---
+++ 34-Remove Duplicates from Sorted List.js
@@ -0,0 +1,7 @@
+// Problem : Remove Duplicates from Sorted List
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
+// Return the linked list sorted as well.
+
+// Solution :
+// Approach 1- Iterative
+// Approach 2- 
