4ad92162633827f1b865d8b196cdeaeeaf241ee9
Sachin <007mishrasachinmishra@gmail.com>
2024-08-09 23:25:48 +0530

    35

---
+++ 35-Sort a linked list of 0s, 1s and 2s.js
@@ -0,0 +1,7 @@
+// problem : Sort a linked list of 0s, 1s and 2s
+// Given a linked list of 0s, 1s and 2s, The task is to sort and print it.
+
+// Examples: 
+
+// Input: 1 -> 1 -> 2 -> 0 -> 2 -> 0 -> 1 -> NULL 
+// Output: 0 -> 0 -> 1 -> 1 -> 1 -> 2 -> 2 -> NULL
