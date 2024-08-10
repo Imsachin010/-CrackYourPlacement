413d55923a1d13a21c43a207ae08653b9cde7cb1
Sachin <007mishrasachinmishra@gmail.com>
2024-08-10 10:17:22 +0530

    36

---
+++ 36-Remove Linked List Elements.js
@@ -0,0 +1,7 @@
+// Problem : Remove Linked List Elements
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+// Solution - 
+// Approach 1- Iterate through the nodes and remove the value if it matches with given val
+
+
