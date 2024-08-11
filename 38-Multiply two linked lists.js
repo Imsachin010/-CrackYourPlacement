868b1dd48a5641004f7d4fe9dded7623750b0753
Sachin <007mishrasachinmishra@gmail.com>
2024-08-11 13:43:13 +0530

    approach

--- 38-Multiply two linked lists.js
+++ 38-Multiply two linked lists.js
@@ -1,3 +1,7 @@
 // Problem : Multiply two linked lists
 // Given elements as nodes of the two singly linked lists. 
 // The task is to multiply these two linked lists, say L1 and L2.
+
+//solution 
+// approach 1 : convert the LL into integers and then multiply numbers to getthe result and then convert the result into linked list
+// approach 2 : multiply each node of the linked list and then add the result to get the final result
