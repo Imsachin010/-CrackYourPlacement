ab8af0872cd8c0715d0246771ab3294761f03f73
Sachin <007mishrasachinmishra@gmail.com>
2024-08-12 08:19:44 +0530

    approaches

--- 39- Intersection of Two Linked Lists.js
+++ 39- Intersection of Two Linked Lists.js
@@ -1,3 +1,7 @@
 // Intersection of two ll
 // Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
 // If the two linked lists have no intersection at all, return null.
+//SOLUTION 
+// Approach 1 : Traversing both the LL and then matching the nodes
+// Approach 2 : Using Hashing
+// Approach 3 : Using two pointers
