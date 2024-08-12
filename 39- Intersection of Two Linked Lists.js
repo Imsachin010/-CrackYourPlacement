c4c7415e13e5e81f6efdb8bfdaf4e5c3db01b72b
Sachin <007mishrasachinmishra@gmail.com>
2024-08-12 08:36:23 +0530

    solved with ex

--- 39- Intersection of Two Linked Lists.js
+++ 39- Intersection of Two Linked Lists.js
@@ -4,4 +4,25 @@
 //SOLUTION 
 // Approach 1 : Traversing both the LL and then matching the nodes
 // Approach 2 : Using Hashing
-// Approach 3 : Using two pointers
+// Approach 3 : Using two pointers and any pointer which eaches to null just change the pointer to the head of the other LL and then traverse both the LL and then matching the nodes
+// Approach4 : using two dummy nodes and finding length then taking the difference and then traversing the longer LL by the difference and then traversing both the LL and then matching the nodes
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    // using two pointer 
+    if(!headA || !headB) return null;
+    let a = headA;
+    let b = headB;
+
+    while(a !== b){
+        a = a === null ? headB : a.next;
+        b = b === null ? headA : b.next;
+    }
+    return a;
+};
+
+console.log(getIntersectionNode([4,1,8,4,5],[5,6,1,8,4,5])) // [8,4,5]
