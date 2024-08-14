31956187b4feb62dcd5164bb49ffd5980cb1877f
Sachin <007mishrasachinmishra@gmail.com>
2024-08-14 08:20:35 +0530

    problem 40

--- 40- Delete nodes having greater value on right.js
+++ 40- Delete nodes having greater value on right.js
@@ -1,2 +1,5 @@
 // Problem : Delete nodes having greater value on right
-// 
+// Given a singly linked list, remove all the nodes with any node on their right whose value is greater and return the head of the modified linked list.  
+// (Not just the immediate Right, but the entire List on the Right
+
+// Solution
