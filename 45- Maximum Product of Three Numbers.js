b25fa92606daf6ecee6db40a8dde966d40d6cc85
Sachin <007mishrasachinmishra@gmail.com>
2024-08-20 19:52:57 +0530

    solved

--- 45- Maximum Product of Three Numbers.js
+++ 45- Maximum Product of Three Numbers.js
@@ -1,2 +1,9 @@
 // problem : Maximum Product of Three Numbers
 // Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+// Solution 
+var maximumProduct = function(nums) {
+    
+    nums.sort((a,b) => a-b);
+    let n = nums.length;
+    return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1])
+};
