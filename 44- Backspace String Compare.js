62a548dd626e6c8da2fa39044c49af222a777900
Sachin <007mishrasachinmishra@gmail.com>
2024-08-20 19:58:56 +0530

    solution approach

--- 44- Backspace String Compare.js
+++ 44- Backspace String Compare.js
@@ -1,2 +1,6 @@
 // Problem : Backspace String Compare
 // Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+// Solution:
+// Approach 1 :traversing each character, also checking #to reconstruct the string and then comparing them 
+// Approach 2 : Using stack to store the characters and then comparing them
+
