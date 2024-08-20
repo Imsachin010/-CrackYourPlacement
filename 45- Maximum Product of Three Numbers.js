e5a23cd6deb0382837f271575c6538579edff563
Sachin <007mishrasachinmishra@gmail.com>
2024-08-20 19:53:21 +0530

    example

--- 45- Maximum Product of Three Numbers.js
+++ 45- Maximum Product of Three Numbers.js
@@ -7,3 +7,4 @@ var maximumProduct = function(nums) {
     let n = nums.length;
     return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1])
 };
+console.log(maximumProduct([1,2,3])) // 6
