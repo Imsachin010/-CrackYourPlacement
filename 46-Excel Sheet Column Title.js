2c320b76eb11acd9e7c6750811d13c42172c29e9
Sachin <007mishrasachinmishra@gmail.com>
2024-08-21 09:17:31 +0530

    solved with Ex

--- 46-Excel Sheet Column Title.js
+++ 46-Excel Sheet Column Title.js
@@ -1,2 +1,17 @@
 // Problem : Excel Sheet Column Title
 // Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+// Solution 
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    let res = '';
+    while(columnNumber> 0){
+       let rem = (columnNumber - 1 )%26;
+       res = String.fromCharCode('A'.charCodeAt(0) + rem ) + res;
+       columnNumber = Math.floor((columnNumber - 1 ) / 26)
+    }
+    return res;
+};
+console.log(convertToTitle(1)) // A
